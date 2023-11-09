8cc4838e4338eab033e7f27f97ee2fb55cf4f742
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-09 23:33:49 +0300

    Automapper implantation

---
+++ Services/Catolog/LearnHub.Services.Catolog/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LearnHub.Services.Catolog.Dtos;
+using LearnHub.Services.Catolog.Models;
+
+namespace LearnHub.Services.Catolog.Mapping;
+
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Course, CourseDto>().ReverseMap();
+        CreateMap<Course, CourseCreateDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Feature, FeatureDto>().ReverseMap();
+    }
+}
